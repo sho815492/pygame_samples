99310bd953d43e051422749ddeb4530881e28fe0
sho815492 <187017041+sho815492@users.noreply.github.com>
2025-05-10 22:14:44 +0900

    DEMO1

--- demo_01.py
+++ demo_01.py
@@ -4,8 +4,8 @@ from pygame.locals import Rect
 pygame.init()
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode([640, 480])
-pygame.display.set_caption("pygame demo - window title here")
+screen = pygame.display.set_mode([700, 600])
+pygame.display.set_caption("pygame demo - aiueo")
 
 running = True
 x1, y1 = 0, 2
@@ -17,12 +17,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((238, 238, 170))  # back ground color
+    screen.fill((100, 100, 170))  # back ground color
 
-    pygame.draw.circle(screen, (176, 176, 222), (320, 240), 120)
+    pygame.draw.circle(screen, (150 , 176, 222), (400, 300), 150)
+    pygame.draw.circle(screen, (222, 176, 222), (150, 300), 20)
     pygame.draw.circle(screen, (222, 176, 222), (120, 120), 20)
-    pygame.draw.circle(screen, (222, 176, 222), (120, 120), 20)
-    pygame.draw.rect(screen, (120, 120, 120), Rect(120, 120, 200, 120))
+    pygame.draw.rect(screen, (255, 120, 100), Rect(200, 120, 200, 120))
+    pygame.draw.ellipse(screen, (100,200,100),(200,200,150,50,))
 
     color_on = (240, 120, 120)
     color_off = (120, 120, 120)
@@ -36,6 +37,9 @@ while running:
     x1 += 1
     if x1 > 4:
         x1 = 0
+        y1 += 1
+        if y1 > 6:
+         y1 = 0
 
     pygame.display.flip()  # update
     clock.tick(5)  # FPS, Frame Per Second
